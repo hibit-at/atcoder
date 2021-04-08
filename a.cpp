5e8cfb4c22fb598dfb962a542790246a00d92859
unknown <hibit_at@hotmail.com>
2021-04-08 19:37:24 +0900

    hoge

--- a.cpp
+++ a.cpp
@@ -14,35 +14,25 @@ using namespace std;
 #define all(A) A.begin(), A.end()
 typedef long long ll;
 
-int main_test(int n){
-  // ll n;
-  // cin >> n;
-  if(n == 1){
-    cout << "Aoki" << endl;
-    return 0;
-  }
-  bool isOK = true;
-  ll low_limit = n/2 + 1;
-  while(low_limit > 3){
-    low_limit /= 2;
-    isOK = !isOK;
-    // cout << low_limit << endl;
-  }
-  if(isOK){
-    cout << "Takahashi" << endl;
-  }else{
-    if(low_limit == 2){
-      cout << "Aoki" << endl;
-    }else{
-      cout << "Takahashi" << endl;
-    }
-  }
-  return 0;
-}
-
 int main(){
-  rep1(i,64){
-    cout << "N=" << i << ", ";
-    main_test(i);
+  vector<int> score(3);
+  vector<int> compare(3);
+  rep(i,3){
+    int a;
+    cin >> a;
+    score[i] = a;
+    compare[i] = a;
+  }
+  sort(all(compare));
+  rep(i,3){
+    if(score[i] == compare[0]){
+      cout << 3 << endl;
+    }
+    if(score[i] == compare[1]){
+      cout << 2 << endl;
+    }
+    if(score[i] == compare[2]){
+      cout << 1 << endl;
+    }
   }
 }
--- testmake.cpp
+++ testmake.cpp
@@ -1,44 +1,106 @@
-#include <algorithm>
-#include <iostream>
-#include <math.h>
-#include <queue>
-#include <set>
 #include <string>
-#include <utility>
+#ifdef __LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <iomanip>
+#include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
-#define rep(i, n) for (int i = 0; i < n; i++)
-#define rep1(i, n) for (int i = 1; i < n + 1; i++)
-#define all(A) A.begin(), A.end()
-typedef long long ll;
+using ll = long long;
+using P = pair<int, int>;
+using PIL = pair<int, ll>;
+using PLI = pair<ll, int>;
+using PLL = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+using Cost_Graph = vector<vector<PIL>>;
 
-vector<bool> make_erat(int limit) {
-  vector<bool> erat(limit, true);
-  erat[0] = false;
-  erat[1] = false;
-  for (int i = 0; i * i < limit; i++) {
-    if (erat[i]) {
-      int j = i * i;
-      while (j < limit) {
-        erat[j] = false;
-        j += i;
-      }
-    }
+template <class T> bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return 1;
   }
-  return erat;
+  return 0;
+}
+template <class T> bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+#define REP(i, n) for (int i = 0; i < int(n); i++)
+#define ROUNDUP(a, b) ((a + b - 1) / b)
+#define YESNO(T) cout << (T ? "YES" : "NO") << endl
+#define yesno(T) cout << (T ? "yes" : "no") << endl
+#define YesNo(T) cout << (T ? "Yes" : "No") << endl
+
+const int INFint = 1 << 30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007LL;
+const double pi = 3.14159265358979;
+
+string main_test(ll n) {
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  // cout << fixed << setprecision(15);
+
+  // ll n;
+  // cin >> n;
+
+  ll left = (n + 2) / 2, right = n;
+  bool winner = false;
+  // cout << (winner ? "win" : "lose") << " " << left << " " << right << endl;
+  while (left > 1) {
+    if (winner) {
+      right = left - 1;
+      left = (right + 2) / 2;
+    } else {
+      right = left - 1;
+      left = (right + 1) / 2;
+    }
+
+    winner = !winner;
+
+    // cout << (winner ? "win" : "lose") << " " << left << " " << right << endl;
+  }
+
+  if (winner) {
+    return "Takahashi";
+  } else {
+    return "Aoki";
+  }
 }
 
+string main_mine(ll n) {
+  // ll n;
+  // cin >> n;
+  if (n == 1) {
+    return "Aoki";
+  }
+  bool isOK = true;
+  ll low_limit = n / 2 + 1;
+  while (low_limit > 3) {
+    low_limit /= 2;
+    isOK = !isOK;
+    // cout << low_limit << endl;
+  }
+  if (low_limit == 3) {
+    return "Takahashi";
+  }
+  if (low_limit == 2) {
+    if (isOK) {
+      return "Takahashi";
+    } else {
+      return "Aoki";
+    }
+  }
+  return "None";
+}
 
-int main(){
-    vector<bool> erat = make_erat(50);
-    vector<int> primes;
-    rep(i,50){
-        if(erat[i]) primes.push_back(i);
-    }
-    cout << primes.size() << endl;
-    for(int i : primes){
-        cout << i << " ";
-    }
-    cout << endl;
+int main() {
+  for (int i = 1; i <= 32; i++) {
+    cout << i << ":" << main_test(i) << "," << main_mine(i) << endl;
+  }
 }
