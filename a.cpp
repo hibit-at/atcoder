5a5dbbb625972ce8b3d8c7a0ca6e93eb96d606d6
unknown <hibit_at@hotmail.com>
2021-05-01 12:18:33 +0900

    hoge

--- a.cpp
+++ a.cpp
@@ -14,19 +14,30 @@ using namespace std;
 #define rep1(i, n) for (int i = 1; i < n + 1; i++)
 #define all(A) A.begin(), A.end()
 typedef long long ll;
+using namespace std;
 
-ll gcd(ll a, ll b) {
-  if (b == 0)
-    return a;
-  return gcd(b, a % b);
+ll const mod = 1e9 + 7;
+
+void plus_and_mod(ll &a, ll b, ll mod) {
+  a += b;
+  a %= mod;
 }
 
 int main() {
-  ll a,b,c;
-  cin >> a >> b >> c;
-  ll gcd_of_abc = gcd(a,gcd(b,c));
-  a /= gcd_of_abc;
-  b /= gcd_of_abc;
-  c /= gcd_of_abc;
-  cout << (a-1)+(b-1)+(c-1) << endl;
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  string t = "atcoder*";
+  vector<vector<ll>> dp(n + 1, vector<ll>(8, 0));
+  dp[0][0] = 1;
+  rep(i, n) {
+    rep(j, 8) {
+      plus_and_mod(dp[i + 1][j], dp[i][j], mod);
+      if (s[i] == t[j]) {
+        plus_and_mod(dp[i + 1][j + 1], dp[i][j], mod);
+      }
+    }
+  }
+  cout << dp[n][7] % mod << endl;
 }
