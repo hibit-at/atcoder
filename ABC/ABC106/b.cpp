4563a1fafeaab6b0836adb69399d3debbc8d180b
hibit-at <hibit_at@hotmail.com>
2022-02-08 03:48:35 +0900

    0208

---
+++ ABC/ABC106/b.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stack>
+
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i < n + 1; i++)
+#define all(A) A.begin(), A.end()
+#define itr(A, l, r) A.begin() + l, A.begin() + r
+#define debug(var) cout << #var << " = " << var << endl;
+typedef long long ll;
+
+int devisor(int n)
+{
+    int ans = 0;
+    rep1(i, n)
+    {
+        if (n % i == 0)
+        {
+            ans++;
+        }
+    }
+    return ans;
+}
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    rep1(i, n)
+    {
+        if (devisor(i) == 8 && i%2 == 1)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
