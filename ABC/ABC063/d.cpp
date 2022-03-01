ef68047a77690d4ec003f1c8dd0169049b036495
unknown <hibit_at@hotmail.com>
2022-03-02 04:33:15 +0900

    0302

---
+++ ABC/ABC063/d.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stack>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep1(i, n) for (ll i = 1; i < n + 1; i++)
+#define all(A) A.begin(), A.end()
+#define itr(A, l, r) A.begin() + l, A.begin() + r
+#define debug(var) cout << #var << " = " << var << endl;
+typedef long long ll;
+
+template <typename T>
+void prll_vector(vector<T> v)
+{
+    for (T i : v)
+    {
+        cout << i << ',';
+    }
+    cout << endl;
+    return;
+}
+
+bool solve(ll target, vector<ll> h, ll n, ll a, ll b)
+{
+    rep(i, n)
+    {
+        h[i] -= b * target;
+    }
+    // prll_vector(h);
+    a -= b;
+    ll count = 0;
+    rep(i, n)
+    {
+        if (h[i] <= 0)
+        {
+            continue;
+        }
+        count += (h[i] + a - 1) / a;
+    }
+    // debug(count);
+    if (count <= target)
+    {
+        // cout << "possible" << endl;
+        return true;
+    }
+    else
+    {
+        // cout << "impossible" << endl;
+        return false;
+    }
+}
+
+int main(void)
+{
+    ll n, a, b;
+    cin >> n >> a >> b;
+    vector<ll> h(n);
+    rep(i, n)
+    {
+        cin >> h[i];
+    }
+    ll ng = -1;
+    ll ok = 1e9 + 1;
+    while (abs(ng - ok) > 1)
+    // rep(target, 10)
+    {
+        ll target = (ok + ng) / 2;
+        // ll target = 2;
+        // cout << target << " " << solve(target, h, n, a, b) << endl;
+        if (solve(target, h, n, a, b))
+        {
+            ok = target;
+        }
+        else
+        {
+            ng = target;
+        }
+    }
+    cout << ok << endl;
+}
---
+++ ABC/ABC107/d.cpp
@@ -0,0 +1,146 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <stack>
+
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define rep1(i, n) for (ll i = 1; i < n + 1; i++)
+#define all(A) A.begin(), A.end()
+#define itr(A, l, r) A.begin() + l, A.begin() + r
+#define debug(var) cout << #var << " = " << var << endl;
+typedef long long ll;
+
+template <typename T>
+void prll_vector(vector<T> v)
+{
+    for (T i : v)
+    {
+        cout << i << ',';
+    }
+    cout << endl;
+    return;
+}
+
+template <typename S, typename T>
+void prll_map(map<S, T> mp)
+{
+    for (auto p : mp)
+    {
+        cout << "key : " << p.first << ", value : " << p.second << endl;
+    }
+}
+
+struct BIT
+{
+public:
+    vector<ll> bit;
+    ll N;
+
+public:
+    BIT(ll size)
+    {
+        N = size;
+        bit.resize(N + 1);
+    }
+
+    // 一点更新です
+    void add(ll a, ll w)
+    {
+        for (ll x = a; x <= N; x += x & -x)
+            bit[x] += w;
+    }
+
+    // 1~Nまでの和を求める。
+    ll sum(ll a)
+    {
+        ll ret = 0;
+        for (ll x = a; x > 0; x -= x & -x)
+            ret += bit[x];
+        return ret;
+    }
+};
+
+bool solve(ll n, ll target, vector<ll> a)
+{
+    rep(i, n)
+    {
+        if (a[i] >= target)
+        {
+            a[i] = 1;
+        }
+        else
+        {
+            a[i] = -1;
+        }
+    }
+    // prll_vector(a);
+    vector<ll> s(n + 1);
+    rep(i, n)
+    {
+        s[i + 1] = a[i];
+        s[i + 1] += s[i];
+    }
+    // prll_vector(s);
+    BIT bit(2 * n + 2);
+    ll inversion = 0;
+    rep(j, n + 1)
+    {
+        ll offset = n + 1;
+        // prll_vector(bit.bit);
+        inversion += j - bit.sum(s[j] + offset);
+        bit.add(s[j] + offset, 1);
+    }
+    ll m = n * (n + 1) / 2;
+    // debug(inversion);
+    if (inversion <= m / 2)
+    {
+        // cout << "possible" << endl;
+        return true;
+    }
+    else
+    {
+        // cout << "impossible" << endl;
+        return false;
+    }
+}
+
+int main(void)
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n)
+    {
+        cin >> a[i];
+    }
+    ll ok = 0;
+    ll ng = 1e9 + 1;
+    while (abs(ok - ng) > 1)
+    // rep1(target, 40)
+    {
+        // ll target = 10;
+        ll target = (ok + ng) / 2;
+        if (solve(n, target, a))
+        {
+            // cout << target << " possible" << endl;
+            ok = target;
+        }
+        else
+        {
+            // cout << target << " impossible" << endl;
+            ng = target;
+        }
+    }
+    cout << ok << endl;
+}
